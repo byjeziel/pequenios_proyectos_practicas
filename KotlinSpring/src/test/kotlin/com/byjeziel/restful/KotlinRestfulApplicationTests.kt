614c7503afcaaf09d1b5d32fdbcf71ba96d72e17
Jezieel7 <jezielpaladino@gmail.com>
2025-01-29 14:51:57 -0300

    Agregado crud de kotlin con spring

---
+++ KotlinSpring/src/test/kotlin/com/byjeziel/restful/KotlinRestfulApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.byjeziel.restful
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfulApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
